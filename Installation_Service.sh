e8f1cb1d2ba6e9d565d10e0d5f5d03f43d6b6fe6
MarvinElz <Marvin.Elz@gmx.de>
2019-06-20 08:47:32 +0200

    Funktionsfähig, Getestet und aufgeräumt

--- Installation_Service.sh
+++ Installation_Service.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Service kopieren."
 sudo cp CAM_Stream.service /etc/systemd/system/CAM_Stream.service
-sudo cp connect_to_network.service /etc/systemd/system/connect_to_network.service
+#sudo cp connect_to_network.service /etc/systemd/system/connect_to_network.service
 echo "Rechte setzen."
 sudo chown root:root /etc/systemd/system/CAM_Stream.service
-sudo chown root:root /etc/systemd/system/connect_to_network.service
+#sudo chown root:root /etc/systemd/system/connect_to_network.service
 echo "Done."
+echo "connect_to_network.sh muss jetzt noch manuell in rc.local einfügen."
+echo "/etc/rc.local"
+echo "sudo bash /home/pi/CAM_Stream/connect_to_network.sh &"
--- stream.sh
+++ stream.sh
@@ -1,22 +1,11 @@
 #! /bin/bash
-#avconv -i /dev/video0 -deinterlace -vcodec h264_omx -pix_fmt yuv420p -preset medium -r 30 -b:v 5000k -acodec libmp3lame -ar 44100 -threads 4 -qscale 3 -b:a 44100 -bufsize 512k -f flv rtmp://a.rtmp.youtube.com/live2/tyg5-k92t-hj34-062d
-# ffmpeg -i /dev/video0 -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 1000k -acodec libmp3lame -ar 44100 -threads 4 -qscale 3 -b:a 44100 -bufsize 512k -f flv rtmp://a.rtmp.youtube.com/live2/tyg5-k92t-hj34-062d
 sudo modprobe bcm2835-v4l2
+                                                                                                                                                                                                      
+echo "Damit das Video in nahezu 'Echtzeit' läuft, muss dem Server eine höhere Bildrate vorgetäuscht werden."
+echo "Dazu muss -fps 30 (raspivid) kleiner gewählt werden als -framerate 31 (ffmpeg)"
+echo "Dabei kommt es ca. alls 30 Sekunden zu einem Buffering."
+echo "Werden beide Bildraten gleich gewählt, tritt eine Latenz von ca. 5-6 Sekunden auf, aber dafür auch kein Buffering."
 
-
-#raspivid -o - -t 0 -vf -hf -fps 60 -b 6000000 -w 1000 -h 720 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 30 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/tyg5-k92t-hj34-062d
-
-# Server-IP: 192.168.20.108
-
-
-# HD-Ready#                                                                                                                                                                                                      V -b 5000k             
-# raspivid -o - -t 0 -vf -hf -fps 10 -w 1920 -h 1080 -b 3000000 -n | ffmpeg -re  -r 15 -ar 44100 -ac 2 -acodec pcm_s16le -f s16le  -ac 2 -i /dev/zero -f h264 -framerate 10  -i - -vcodec copy -acodec aac -ab 128k -strict experimental  -f flv rtmp://185.230.162.203/live/test
 echo "Starte Video-Stream."
 raspivid -o - -t 0 -vf -hf -fps 30 -w 1920 -h 1080 -b 6000000 -n | ffmpeg -re  -r 15 -ar 44100 -ac 2 -acodec pcm_s16le -f s16le  -ac 2 -i /dev/zero -f h264 -framerate 31  -i - -vcodec copy -acodec aac -ab 128k -strict experimental  -f flv rtmp://185.230.162.203/live/test
-
-
-# Full-HD
-# raspivid -o - -t 0 -vf -hf -fps 30 -w 1920 -h 1080 | ffmpeg -re  -r 15 -ar 44100 -ac 2 -acodec pcm_s16le -f s16le  -ac 2 -i /dev/zero -f h264 -framerate 30  -i - -vcodec copy -acodec aac -ab 128k -b 5000k -strict experimental  -f flv rtmp://192.168.178.26/live/test
-## -vf -g 30 -rtbufsize 1500M  -b:v 5M -maxrate 5M -bufsize 3M
-
 
